27e14b8ed69624044fddcc6b2d0ee3404313b3af
Ryan Propst <ryanpropst@gmail.com>
2019-09-12 16:49:02 -0500

    add new edabit exercise

---
+++ edabitTestEx/triArea/triarea.js
@@ -0,0 +1,21 @@
+/*
+Area of a Triangle
+Requirements:
+_____________________________________________
+Write a function that takes the base
+and height of a triangle and return
+ its area.
+The area of a triangle is: (base * height) / 2
+______________________________________________
+*/
+
+function triArea(base, height) {
+  result = base * height * 0.5;
+  console.log(result);
+}
+
+triArea(3, 2);
+
+triArea(7, 4);
+
+triArea(10, 10);
